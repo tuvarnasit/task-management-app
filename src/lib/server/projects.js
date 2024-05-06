7853e6902519832fcd43bb173eea56d5e070c718
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-05-06 15:52:43 +0300

    Add user id and export an arary for storing the projects

---
+++ src/lib/server/projects.js
@@ -0,0 +1,20 @@
+export let projects = [
+  {
+    id: '9923a1be-ecc4-4974-a00a-9f1229dc78f9',
+    title: '',
+    ownerId: '737b1897-ea08-4c0c-a8dd-c16ea6b073441',
+    collaboratorIds: [],
+    sections: [
+      {
+        id: 'ad98bae3-de56-4742-8c73-e845543cc554',
+        name: '',
+        tasks: [
+          {
+            id: 'c9b3f3ef-1016-40ca-8889-fdcb5f79ba52',
+            name: ''
+          }
+        ]
+      }
+    ]
+  }
+];
--- src/lib/server/users.js
+++ src/lib/server/users.js
@@ -1,5 +1,6 @@
 export let users = [
   {
+    id: '737b1897-ea08-4c0c-a8dd-c16ea6b073441',
     firstName: 'Ivan',
     lastName: 'Ivanov',
     email: 'ivan.ivanov@email.com',
