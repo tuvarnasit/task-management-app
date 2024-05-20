d810126c8b3ae851763eb936e72645fb9565a340
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-05-20 20:34:59 +0300

    Fix dark mode toggle styles

--- src/lib/components/darkModeToggle.svelte
+++ src/lib/components/darkModeToggle.svelte
@@ -5,12 +5,12 @@
 </script>
 
 <div class="flex w-full justify-end">
-  <Button on:click={toggleMode} variant="outline" size="icon">
+  <Button on:click={toggleMode} variant="outline" size="icon" class="relative">
     <Sun
-      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-150 dark:-rotate-90 dark:scale-0"
     />
     <Moon
-      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all duration-150 dark:rotate-0 dark:scale-100"
     />
     <span class="sr-only">Toggle theme</span>
   </Button>
