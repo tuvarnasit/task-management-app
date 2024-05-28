d7f1aa488c4299b2b233e9965524e67173788add
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-05-28 19:11:03 +0300

    Add dummy data

--- src/lib/server/projects.js
+++ src/lib/server/projects.js
@@ -18,5 +18,56 @@ export let projects = [
         ]
       }
     ]
+  },
+  {
+    id: 'tb96jz2od7',
+    title: 'Задачи',
+    ownerId: 'jc2fq8hi2p',
+    isFavorite: false,
+    collaboratorIds: [],
+    sections: [
+      {
+        id: 'gqg66kzizt',
+        name: 'Домакински задължения',
+        tasks: [
+          {
+            id: 'd4nmppat45',
+            name: 'Измий съдовете'
+          },
+          {
+            id: 'wnbyt7p0q9',
+            name: 'Пусни пералня и после простри дрехите'
+          },
+          {
+            id: 't7lzv0iwmo',
+            name: 'Отиди до магазина и купи нещо за хапване',
+            isCompleted: true
+          }
+        ]
+      },
+      {
+        id: 'gdwf34h49f',
+        name: 'Improvement ✨',
+        tasks: [
+          {
+            id: 'izauv11pzl',
+            name: 'Направи тренировка във фитнеса'
+          },
+          {
+            id: 'ua6siolzq5',
+            name: 'Прочети 100 страници'
+          },
+          {
+            id: 's59rathyzr',
+            name: '30 минути медитация',
+            isCompleted: true
+          },
+          {
+            id: 'qk2vdm2s35',
+            name: '1 час свирене на китара'
+          }
+        ]
+      }
+    ]
   }
 ];
